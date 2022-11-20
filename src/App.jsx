2d437fa92af33daecee5bca2d95d7fd521cc63a4
ahanaf <ahanafabdullah9@gmail.com>
2022-11-20 22:30:18 +0600

    added Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import {
+  default as Explore,
+  default as ForgetPassword,
+  default as Offers,
+  default as Profile,
+  default as Signin,
+  default as SignUp,
+} from "./Pages/Explore";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/" element={<Offers />} />
+        <Route path="/forgot-password" element={<ForgetPassword />} />
+      </Routes>
+      <Navbar />
+    </Router>
   );
 };
 
